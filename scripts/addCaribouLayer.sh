5f83abe665598867c4050f47b44bb17322806170
Simon Spannagel <simon.spannagel@cern.ch>
2016-12-09 12:46:56 +0100

    Checkout Xilinx branch "krogoth" by default

--- scripts/addCaribouLayer.sh
+++ scripts/addCaribouLayer.sh
@@ -2,7 +2,7 @@
 cd ../../
 
 #Clone repositirues of the required layers
-git clone git://git.yoctoproject.org/meta-xilinx
+git clone --branch krogoth git://git.yoctoproject.org/meta-xilinx
 
 #Set up bitbake
 . oe-init-build-env
