463f351736d792bdd7b01f8cff8053a62d8212cf
Adrian Fiergolski <adrian.fiergolski@cern.ch>
2017-02-01 18:36:55 +0100

    Enable PR service to check for the newest versions from the git repositories.

--- scripts/addCaribouLayer.sh
+++ scripts/addCaribouLayer.sh
@@ -9,3 +9,8 @@ git clone --branch morty git://git.yoctoproject.org/meta-xilinx
 bitbake-layers add-layer ../meta-xilinx
 bitbake-layers add-layer ../meta-caribou
 sed -i 's/^MACHINE ??= ".*"/MACHINE ??= "caribou-zynq7"/' conf/local.conf
+
+#Enable PR service, in order to always download the latest versions
+#from the git repositories
+echo '#PR service 
+PRSERV_HOST = "localhost:0"' > conf/local.conf
