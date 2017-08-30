114b43ca1be514b6fdaf2dc2322d92df83bf30ac
Adrian Fiergolski <adrian.fiergolski@cern.ch>
2017-08-30 11:06:50 +0200

    Users specifies the target machine name in order to assign the right MAC address. Update to pyro version.

--- scripts/prepare_sd.sh
+++ scripts/prepare_sd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SD_DEVICE_DEFAULT="/dev/mmcblk0"
+DEVICE_NAME_DEFAULT="pclcd-zynq"
 
 #Colours
 ORANGE='\033[0;33m'
@@ -17,13 +18,15 @@ cd ../../
 bitbake parted-native dosfstools-native mtools-native
 
 #Create image
-IMAGE_PATH=$( wic create sdimage-bootpart -e caribou-image | tee /dev/tty | grep "Info: The new image(s) can be found here:" -A1 | sed -n '2p' | sed 's/ //g' )
+IMAGE_PATH=$PWD/$( basename $( wic create sdimage-bootpart -e caribou-image 2>&1 | tee /dev/tty | grep "INFO: The new image(s) can be found here:" -A1 | sed -n '2p' ) )
 
 #Copy image to the SD card
 read -p "Please enter absolute path SD card device [$SD_DEVICE_DEFAULT]: " SD_DEVICE
 SD_DEVICE=${SD_DEVICE:-$SD_DEVICE_DEFAULT}
-
-if sudo dd if=$IMAGE_PATH of=$SD_DEVICE bs=1M ; then
+#Get device name
+read -p "Please enter the target deivce name from list: pclcd-zynq, pclcd-lab-zynq, pclcd-lab-zynq1 [pclcd-zynq]: " DEVICE_NAME
+DEVICE_NAME=${DEVICE_NAME:-$DEVICE_NAME_DEFAULT}
+if sudo dd if=${IMAGE_PATH} of=$SD_DEVICE bs=1M ; then
     #Wait till dd finishes
     printf "${ORANGE}Waiting for coping process to complete...\n"
     sudo sync
@@ -32,5 +35,22 @@ if sudo dd if=$IMAGE_PATH of=$SD_DEVICE bs=1M ; then
 else
     printf "${RED}Coping process failed !\n"
 fi
+#Assign MAC address
+mkdir -p boot
+sudo mount -o umask=000  ${SD_DEVICE_DEFAULT}p1 boot
+case "$DEVICE_NAME" in
+    "pclcd-zynq"      ) mac="00:0A:35:00:01:24";;
+    "pclcd-lab-zynq"  ) mac="00:0A:35:00:01:23";;
+    "pclcd-lab-zynq1" ) mac="00:0A:35:00:01:25";;
+    *                 ) printf "${RED}Unknown device name. Can't assign the MAC address !\n";
+			printf "${NC}\n";
+			rm -R boot;
+			rm $IMAGE_PATH;
+			exit 1;;
+esac
+echo "ethaddr=${mac}" >> boot/uEnv.txt
+sudo umount boot
+rm -R boot
+rm $IMAGE_PATH
 printf "${NC}\n"
 
