e96dcefd78456f0e7ce2fdf69c5899ccc00f82e4
Adrian Fiergolski <adrian.fiergolski@cern.ch>
2017-11-01 16:56:24 +0100

    Make addCaribouLayer.sh script more generic.

--- scripts/addCaribouLayer.sh
+++ scripts/addCaribouLayer.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-cd ../../
+# The script adds meta-caribou layer with its dependencies to the poky framework.
+# It assumes that meta-caribou has been cloned to the 'poky' directory.
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $SCRIPT_PATH/../../
 
 #Clone repositirues of the required layers
 git clone --branch pyro git://git.yoctoproject.org/meta-xilinx
