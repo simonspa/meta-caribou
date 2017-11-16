651bfb30de9eeacb76295e58e8fc58ab8f7341f5
Adrian Fiergolski <adrian.fiergolski@cern.ch>
2017-11-16 15:13:12 +0100

    Gitlab CI uses executable docker image.

--- misc/docker/build-caribou-image.sh
+++ misc/docker/build-caribou-image.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 export IMAGE_PATH=/builds/poky/wic
-git clone https://gitlab.cern.ch/Caribou/meta-caribou.git /builds/poky/meta-caribou
+export GIT_PATH=/builds/poky/meta-caribou
+git clone ${CI_REPOSITORY_URL} ${GIT_PATH}
+git --git-dir=${GIT_PATH}/.git --work-tree=${GIT_PATH} checkout ${CI_COMMIT_SHA}
 cd /builds/poky
 source meta-caribou/scripts/addCaribouLayer.sh GIT_CI
 .  oe-init-build-env
 bitbake caribou-image
 bitbake wic-tools
 IMAGE_FULL_PATH=$( wic create sdimage-bootpart -e caribou-image -o ${IMAGE_PATH} 2>&1 |  grep "INFO:\ The\ new image(s)\ can\ be\ found\ here:" -A1 |  sed -n '2p' )
+mv $IMAGE_FULL_PATH ${IMAGE_PATH}/sdimage-${CI_COMMIT_SHA:0:8}-mmcblk.direct
