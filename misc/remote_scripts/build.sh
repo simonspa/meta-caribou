3a45af704b0b428f4a6edc214713ce79ef44ef18
Adrian Fiergolski <adrian.fiergolski@cern.ch>
2017-11-16 19:39:45 +0100

    Add script building meta-caribou image locally using a docker image.

---
+++ misc/remote_scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#The script builds locally the meta-caribou image using a docker container from remote gitlab repository.
+#It uses the latest master version of meta-caribou.
+#The script creates/reuses 'downloads' and 'sstate-cache' directories to store the poky sources and cache the intermediate stages.
+#It allows to speed up the build process if the script is called again in the same directory.
+
+export IMAGE_PATH=/builds/poky/wic
+export DOCKER_IMAGE=gitlab-registry.cern.ch/caribou/meta-caribou
+export DOCKER_CONTAINER=meta-caribou-container
+
+docker pull gitlab-registry.cern.ch/caribou/meta-caribou
+if [ ! -d downloads ]; then
+    mkdir downloads
+fi
+
+if [ ! -d sstate-cache ]; then
+    mkdir sstate-cache
+fi
+
+if [ ! "$(docker ps -q -f name=${DOCKER_CONTAINER})" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=${DOCKER_CONTAINER})" ]; then
+        # cleanup
+        docker rm ${DOCKER_CONTAINER}
+    fi
+fi
+docker run -it --net=host -v ${PWD}/downloads:/downloads -v  ${PWD}/sstate-cache:/sstate-cache --name=${DOCKER_CONTAINER} ${DOCKER_IMAGE}:latest
+docker cp ${DOCKER_CONTAINER}:${IMAGE_PATH}/ .
