1aa91c48e7a6b915ab8786eaccd2c4f21d05e178
Simon Spannagel <simon.spannagel@cern.ch>
2018-06-20 15:06:20 +0200

    Update SD card preparation script to allow different boot partition names

--- scripts/prepare_sd.sh
+++ scripts/prepare_sd.sh
@@ -24,21 +24,38 @@ IMAGE_PATH=$PWD/$( basename $( wic create sdimage-bootpart -e ${IMAGE_NAME} 2>&1
 #Copy image to the SD card
 read -p "Please enter absolute path SD card device [$SD_DEVICE_DEFAULT]: " SD_DEVICE
 SD_DEVICE=${SD_DEVICE:-$SD_DEVICE_DEFAULT}
-#Get device name
-read -p "Please enter the target deivce name from list: pclcd-lab-zynq, pclcd-testbeam-zynq, pclcd-zynqX [$DEVICE_NAME_DEFAULT]: " DEVICE_NAME
+
+# Get device name
+read -p "Please enter the target device name from list: pclcd-lab-zynq, pclcd-testbeam-zynq, pclcd-zynqX [$DEVICE_NAME_DEFAULT]: " DEVICE_NAME
 DEVICE_NAME=${DEVICE_NAME:-$DEVICE_NAME_DEFAULT}
+
 if sudo dd if=${IMAGE_PATH} of=$SD_DEVICE bs=1M ; then
-    #Wait till dd finishes
+    # Wait until dd finishes
     printf "${ORANGE}Waiting for copying process to complete...\n"
     sudo sync
 
-    printf "${GREEN}Done.\n"
+    printf "${GREEN}Copying process succeeded.\n"
 else
-    printf "${RED}Copying process failed !\n"
+    printf "${RED}Copying process failed!\n"
+    exit 1
 fi
-#Assign MAC address
+
+# Figure out boot partition name:
+if [ -e "${SD_DEVICE}p1" ]; then
+    SD_DEVICE_BOOT_PARTITION=${SD_DEVICE}p1
+elif [ -e "${SD_DEVICE}1" ]; then
+    SD_DEVICE_BOOT_PARTITION=${SD_DEVICE}1
+else
+    printf "${RED}Could not find boot partition on device ${SD_DEVICE}.\n"
+    exit 1
+fi
+printf "${ORANGE}Found boot partition at ${SD_DEVICE_BOOT_PARTITION}${NC}\n"
+
+# Assign MAC address
 mkdir -p boot
-sudo mount -o umask=000  ${SD_DEVICE_DEFAULT}p1 boot
+sudo mount -o umask=000  ${SD_DEVICE_BOOT_PARTITION} boot
+
+# Figure out the MAC address
 case "$DEVICE_NAME" in
     "pclcd-lab-zynq"       ) mac="00:0A:35:00:02:00";;
     "pclcd-testbeam-zynq"  ) mac="00:0A:35:00:02:01";;
@@ -50,9 +67,12 @@ case "$DEVICE_NAME" in
 			rm $IMAGE_PATH;
 			exit 1;;
 esac
+
 echo "ethaddr=${mac}" >> boot/uEnv.txt
 sudo umount boot
 rm -R boot
 rm $IMAGE_PATH
+
+printf "${GREEN}Finished SD card preparation."
 printf "${NC}\n"
 
